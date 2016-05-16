0bd0c52b7816add2422ad43e1285c48d499d6463
bitreight <alexey.kur@gmail.com>
2016-05-16 20:39:32 +0300

    Added methods for checking whether username and password are correct. Added error messages in invalidLabel.

--- src/main/java/com/bitreight/taskmanager/controllers/LoginController.java
+++ src/main/java/com/bitreight/taskmanager/controllers/LoginController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -12,8 +13,6 @@ import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 
 public class LoginController {
-    private static final String pathToAdminScreen = "/fxml/Admin.fxml";
-    private static final String pathToUserScreen = "/fxml/User.fxml";
 
     @FXML
     private TextField usernameField;
@@ -25,11 +24,54 @@ public class LoginController {
     private Button loginButton;
 
     @FXML
+    private Label invalidLabel;
+
+    @FXML
     private void loginButtonAction() throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        if(!checkUsername(username)) {
+            invalidLabel.setText("Логин пользователя должен содержать 3..10 символов");
+        }
+        else if (!checkPassword(password)) {
+            invalidLabel.setText("Пароль пользователя должен содержать 3..10 символов");
+        }
+        else {
+            loadAdmin();
+        }
+    }
+
+    @FXML
+    public void textFieldsClick() {
+        invalidLabel.setText("");
+    }
+
+    private void loadAdmin() throws IOException {
+        final String pathToAdminScreen = "/fxml/Admin.fxml";
         Parent root = FXMLLoader.load(getClass().getResource(pathToAdminScreen));
         Scene scene = new Scene(root);
         Stage stage = (Stage) loginButton.getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void loadUser() throws IOException {
+        final String pathToUserScreen = "/fxml/User.fxml";
+        Parent root = FXMLLoader.load(getClass().getResource(pathToUserScreen));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //testable
+    private boolean checkUsername(String username) {
+        return !(username.length() < 3 | username.length() > 10);
+
+    }
+
+    //testable
+    private boolean checkPassword(String password) {
+        return !(password.length() < 3 | password.length() > 10);
     }
 }
