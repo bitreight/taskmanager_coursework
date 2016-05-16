ff004864ba4a65ff2b6b0368f5909e5f39cc08ed
bitreight <alexey.kur@gmail.com>
2016-05-16 19:06:39 +0300

    Added simple switching to Admin UI.

--- src/main/java/com/bitreight/taskmanager/controllers/LoginController.java
+++ src/main/java/com/bitreight/taskmanager/controllers/LoginController.java
@@ -1,15 +1,36 @@
 package com.bitreight.taskmanager.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
 
 public class LoginController {
+    private static final String pathToAdminScreen = "/fxml/Admin.fxml";
+    private static final String pathToUserScreen = "/fxml/User.fxml";
+
     @FXML
     private TextField usernameField;
 
     @FXML
     private PasswordField passwordField;
 
+    @FXML
+    private Button loginButton;
 
+    @FXML
+    private void loginButtonAction() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(pathToAdminScreen));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
