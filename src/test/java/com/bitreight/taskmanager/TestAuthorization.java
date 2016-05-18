5255d1a38bd1fd56e5400dede37254aacd0275f9
bitreight <alexey.kur@gmail.com>
2016-05-18 18:31:00 +0300

    Added TestAuthorization class with one test, works fine.

---
+++ src/test/java/com/bitreight/taskmanager/TestAuthorization.java
@@ -0,0 +1,26 @@
+package com.bitreight.taskmanager;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import static org.junit.Assert.assertTrue;
+
+@ContextConfiguration("classpath:spring/spring-context.xml")
+public class TestAuthorization extends AbstractJUnit4SpringContextTests {
+
+    @Autowired
+    private Authorization auth;
+
+    @Test
+    public void testAuthDefaultAdmin()  {
+        String username = "admin";
+        String password = "password";
+
+        AuthResult authResultExpected = auth.login(username, password);
+
+        assertTrue(authResultExpected.isValid);
+        assertTrue(authResultExpected.isAdmin);
+    }
+}
