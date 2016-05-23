5012fd10c8b6324612ec33c918c119b999d84a25
bitreight <alexey.kur@gmail.com>
2016-05-24 01:48:03 +0300

    Added stored procedures init. Figured out how to use stored functions.

--- src/main/java/com/bitreight/taskmanager/repository/dao/TaskDaoImpl.java
+++ src/main/java/com/bitreight/taskmanager/repository/dao/TaskDaoImpl.java
@@ -1,12 +1,79 @@
 package com.bitreight.taskmanager.repository.dao;
 
 import com.bitreight.taskmanager.model.Task;
-import javafx.collections.ObservableList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
 public class TaskDaoImpl implements TaskDao {
 
+    private JdbcTemplate storedFunctionCall;
+
+    private SimpleJdbcCall procCreateTask;
+    private SimpleJdbcCall procUpdateTask;
+    private SimpleJdbcCall procDeleteTask;
+    private SimpleJdbcCall procAssignTask;
+    private SimpleJdbcCall procDeassignTask;
+    private SimpleJdbcCall procSetTaskCompletionByUser;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.procCreateTask = new SimpleJdbcCall(dataSource)
+                .withProcedureName("addTask");
+
+        this.procUpdateTask = new SimpleJdbcCall(dataSource)
+                .withProcedureName("updateTask");
+
+        this.storedFunctionCall = new JdbcTemplate(dataSource);
+
+//        this.funcGetTasksByDeveloper = new SimpleJdbcCall(dataSource)
+//                .withFunctionName("getTasksByDeveloper")
+//                .returningResultSet("tasksOfDeveloper", new RowMapper<Task>() {
+//                    @Override
+//                    public Task mapRow(ResultSet rs, int i) throws SQLException {
+//
+//                        Task task = new Task();
+//                        task.setId(rs.getInt("id"));
+//                        task.setNumber(rs.getInt("number"));
+//                        task.setName(rs.getString("task_name"));
+//                        task.setDescription(rs.getString("description"));
+//                        task.setDescription(rs.getString("description"));
+//                        task.setDeadline(rs.getDate("deadline"));
+//                        task.setPriority(rs.getInt("priority"));
+//                        task.setIsCompleted(rs.getBoolean("is_completed"));
+//
+//                        return task;
+//                    }
+//                });
+
+        this.procDeleteTask = new SimpleJdbcCall(dataSource)
+                .withProcedureName("deleteTask");
+
+        this.procAssignTask = new SimpleJdbcCall(dataSource)
+                .withProcedureName("assignTask");
+
+        this.procDeassignTask = new SimpleJdbcCall(dataSource)
+                .withProcedureName("deassignTask");
+
+        this.procSetTaskCompletionByUser = new SimpleJdbcCall(dataSource)
+                .withProcedureName("setCompletionByUser");
+    }
+
     @Override
-    public void addTask(Task task) {
+    public void createTask(Task task) {
 
     }
 
@@ -16,8 +83,38 @@ public class TaskDaoImpl implements TaskDao {
     }
 
     @Override
-    public ObservableList<Task> getTasksByDeveloper(int developerId) {
+    public List<Task> getTasks() {
         return null;
+    }
+
+    @Override
+    public List<Task> getTasksByDeveloper(int developerId) {
+        Map out = null;
+
+        final String funcQuery = "Select * From getTasksByDeveloper(?)";
+
+        SqlParameterSource in = new MapSqlParameterSource()
+                .addValue("dev_id", developerId);
+
+        List<Task> tasks = storedFunctionCall.query(funcQuery, new Object[]{developerId}, new RowMapper<Task>() {
+                    @Override
+                    public Task mapRow(ResultSet rs, int i) throws SQLException {
+
+                        Task task = new Task();
+                        task.setId(rs.getInt("id"));
+                        task.setNumber(rs.getInt("number"));
+                        task.setName(rs.getString("task_name"));
+                        task.setDescription(rs.getString("description"));
+                        task.setDescription(rs.getString("description"));
+                        task.setDeadline(rs.getDate("deadline"));
+                        task.setPriority(rs.getInt("priority"));
+                        task.setIsCompleted(rs.getBoolean("is_completed"));
+
+                        return task;
+                    }
+                });
+
+        return tasks;
     }
 
     @Override
