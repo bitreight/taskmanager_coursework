fff6a8a4e7b6cc19cb0fc3127b43122e3c2bc011
bitreight <alexey.kur@gmail.com>
2016-05-24 02:10:07 +0300

    Created simple test for checking if table-valued function getTasksByDeveloper works properly.

---
+++ src/test/java/com/bitreight/taskmanager/TestTaskDao.java
@@ -0,0 +1,26 @@
+package com.bitreight.taskmanager;
+
+import com.bitreight.taskmanager.model.Task;
+import com.bitreight.taskmanager.repository.dao.TaskDao;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import java.util.List;
+
+@ContextConfiguration("classpath:spring/spring-context.xml")
+public class TestTaskDao extends AbstractJUnit4SpringContextTests {
+
+    @Autowired
+    private TaskDao taskDao;
+
+    @Test
+    public void testGetTasksByDeveloper() {
+
+        List<Task> tasks = taskDao.getTasksByDeveloper(2);
+        for(Task task : tasks) {
+            System.out.println(task.getId());
+        }
+    }
+}
