4297ffe87fa14df3da440bc0e8073ebc08dc95f0
bitreight <alexey.kur@gmail.com>
2016-05-28 15:23:57 +0300

    DeveloperDaoImpl implements DeveloperDao with empty methods.

---
+++ src/main/java/com/bitreight/taskmanager/repository/dao/DeveloperDaoImpl.java
@@ -0,0 +1,28 @@
+package com.bitreight.taskmanager.repository.dao;
+
+import com.bitreight.taskmanager.model.Developer;
+
+import java.util.List;
+
+public class DeveloperDaoImpl implements DeveloperDao {
+
+    @Override
+    public int createDeveloper(Developer developer) {
+        return 0;
+    }
+
+    @Override
+    public void updateDeveloper(Developer developer) {
+
+    }
+
+    @Override
+    public List<Developer> getDevelopers() {
+        return null;
+    }
+
+    @Override
+    public void deleteDeveloper(int developerId) {
+
+    }
+}
