bb8061a732d1bd38d574d39177e27571844f0b65
bitreight <alexey.kur@gmail.com>
2016-05-29 00:29:55 +0300

    String fields are initialized by default. Password type is changed to string. Implemented equals() and hashCode().

--- src/main/java/com/bitreight/taskmanager/model/Developer.java
+++ src/main/java/com/bitreight/taskmanager/model/Developer.java
@@ -3,19 +3,15 @@ package com.bitreight.taskmanager.model;
 public class Developer {
 
     private int id;
-    private String username;
-    private byte[] password = new byte[32];
-    private String name;
-    private String surname;
-    private String patronymic;
-    private String position;
+    private String username = "";
+    private String password = "";
+    private String name = "";
+    private String surname = "";
+    private String patronymic = "";
+    private String position = "";
     private boolean isAdmin;
 
-    public Developer() {
-
-    }
-
-    public int getId() {
+   public int getId() {
         return id;
     }
 
@@ -31,11 +27,11 @@ public class Developer {
         this.username = username;
     }
 
-    public void setPassword(byte[] password) {
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public byte[] getPassword() {
+    public String getPassword() {
         return password;
     }
 
@@ -85,5 +81,36 @@ public class Developer {
             return surname + " " + name.charAt(0) + "." + patronymic.charAt(0) + ".";
         }
         return "";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Developer developer = (Developer) o;
+
+        if (id != developer.id) return false;
+        if (isAdmin != developer.isAdmin) return false;
+        if (!username.equals(developer.username)) return false;
+        if (!password.equals(developer.password)) return false;
+        if (!name.equals(developer.name)) return false;
+        if (!surname.equals(developer.surname)) return false;
+        if (!patronymic.equals(developer.patronymic)) return false;
+        return position.equals(developer.position);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + surname.hashCode();
+        result = 31 * result + patronymic.hashCode();
+        result = 31 * result + position.hashCode();
+        result = 31 * result + (isAdmin ? 1 : 0);
+        return result;
     }
 }
