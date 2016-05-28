a52d7c9b651be24b1937e6cfe0d6e936e70e56fb
bitreight <alexey.kur@gmail.com>
2016-05-28 04:30:42 +0300

    Completed Developer model.

--- src/main/java/com/bitreight/taskmanager/model/Developer.java
+++ src/main/java/com/bitreight/taskmanager/model/Developer.java
@@ -2,5 +2,88 @@ package com.bitreight.taskmanager.model;
 
 public class Developer {
 
+    private int id;
+    private String username;
+    private byte[] password = new byte[32];
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String position;
+    private boolean isAdmin;
 
+    public Developer() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(byte[] password) {
+        this.password = password;
+    }
+
+    public byte[] getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    @Override
+    public String toString() {
+        if(surname != null && name != null && patronymic != null) {
+            return surname + " " + name.charAt(0) + "." + patronymic.charAt(0) + ".";
+        }
+        return "";
+    }
 }
